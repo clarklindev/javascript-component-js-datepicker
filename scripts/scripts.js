a87fcb5d4b5b9d386ed9496c51c79adf02ab9e8f
clarklindev <clarklindev@gmail.com>
2019-09-09 13:39:08 +0200

    generateWeekdays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,25 @@
 let datepicker = document.querySelector('input[name="date-input"]');
 let calendar = document.querySelector('.calendar');
+let daysOfWeekLabels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+generateWeekdays();
+generateDaysOfMonth();
+
+//creates the header for days of week
+function generateWeekdays(){
+    let daysOfWeek = document.querySelector('.calendar-daysofweek');
+    for(let i=0; i< daysOfWeekLabels.length; i++){
+        let day = document.createElement('td');
+        let dayText = document.createTextNode(daysOfWeekLabels[i]);
+        day.appendChild(dayText);
+        daysOfWeek.appendChild(day);
+    }
+}
+
+//creates the days of the month
+function generateDaysOfMonth(){
+
+}
+
 datepicker.onclick = function(){
     console.log('onChooseDate');
     calendar.classList.toggle('active');
