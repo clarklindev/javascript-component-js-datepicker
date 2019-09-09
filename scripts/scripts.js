d05ab20c2d03404d7742eddedc925cfec92a9a6b
clarklindev <clarklindev@gmail.com>
2019-09-09 14:11:27 +0200

    populate with days of week

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,13 @@
 let datepicker = document.querySelector('input[name="date-input"]');
 let calendar = document.querySelector('.calendar');
-let daysOfWeekLabels = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+let daysOfWeekLabels = {0:'mon', 1:'tue', 2:'wed', 3:'thu', 4:'fri', 5:'sat', 6:'sun'};
 generateWeekdays();
 generateDaysOfMonth();
 
 //creates the header for days of week
 function generateWeekdays(){
     let daysOfWeek = document.querySelector('.calendar-daysofweek');
-    for(let i=0; i< daysOfWeekLabels.length; i++){
+    for(let i=0; i< Object.keys(daysOfWeekLabels).length; i++){
         let day = document.createElement('td');
         let dayText = document.createTextNode(daysOfWeekLabels[i]);
         day.appendChild(dayText);
@@ -17,6 +17,20 @@ function generateWeekdays(){
 
 //creates the days of the month
 function generateDaysOfMonth(){
+    //rows = 5 (weeks) in month
+    let daysOfMonth = document.querySelector('.calendar-daysofmonth');
+    for(let i=0; i<= 4; i++){
+        let row = document.createElement('tr');
+
+        //populate month from 1st on that day of week
+        for(let j=1; j<=7;j++){
+            let day = document.createElement('td');
+            let dayText = document.createTextNode(j + i*7);
+            day.appendChild(dayText);
+            row.appendChild(day);
+        }
+        daysOfMonth.appendChild(row);
+    }
 
 }
 
